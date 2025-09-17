14839df98ff46b44f77654e5c562d10253dfaaa2
Alexandr Melnikov <kimbar342@gmail.com>
2025-09-17 21:22:19 +0300

    task factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 }
