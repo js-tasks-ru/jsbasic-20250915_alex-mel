ac106f783cbda20c00332d7784cca7ba3e8995cb
Alexandr Melnikov <kimbar342@gmail.com>
2025-10-17 00:33:49 +0300

    4 - module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,51 @@
 function highlight(table) {
-  console.log(table.rows[1].cells[2].dataset);
+  const ageList = [];
+  const genderList = [];
+  const statusList = [];
+  const rows = table.getElementsByTagName('TR');
+  const ageStop = 18;
+
+
+  for (let i = 1; i < rows.length; i++) {
+    for (let j = 1; j < rows[i].cells.length; j++) {
+      if (j === 1) {
+        ageList.push(rows[i].cells[j]);
+      }
+      if (j === 2) {
+        genderList.push(rows[i].cells[j]);
+      }
+      if (j === 3) {
+        statusList.push(rows[i].cells[j]);
+      }
+
+    }
+  }
+  ageList.forEach(el => {
+    if (isFinite(el.innerText)) {
+      let elemAge = Number.parseInt(el.innerText);
+      if (elemAge < ageStop) {
+        el.parentElement.style.textDecoration = 'line-through';
+      }
+    }
+  });
+  genderList.forEach(el => {
+    if (el.innerText === 'm') {
+      el.parentElement.classList.add('male');
+    }
+    if (el.innerText === 'f') {
+      el.parentElement.classList.add('female');
+    }
+  });
+
+  statusList.forEach(el => {
+    if (!('available' in el.dataset)) {
+      el.parentElement.hidden = true;
+    }
+    if (el.dataset.available === 'true') {
+      el.parentElement.classList.add('available');
+    } else {
+      el.parentElement.classList.add('unavailable');
+    }
+  });
 }
+
